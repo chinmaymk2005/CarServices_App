b1eab9e4e8e6ea54782e13a24db01ecf204abb2a
chinmaymk2005 <chinmaymk13@gmail.com>
2025-06-14 14:57:09 +0530

    final commit2

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'https://your-vercel-url.vercel.app', // Adjust this to your frontend URL
+    origin: 'https://car-services-app.vercel.app', // Adjust this to your frontend URL
     credentials: true // Allow credentials (cookies, authorization headers, etc.)
 }))
 app.use(express.json());
